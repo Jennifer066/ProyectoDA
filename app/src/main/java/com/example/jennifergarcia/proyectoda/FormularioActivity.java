87dd6067cd90818100cc6fc56720474742bbc997
Jennifer066 <60203940+Jennifer066@users.noreply.github.com>
2020-12-10 00:26:02 -0600

    Nuevas actualizaciones

--- app/src/main/java/com/example/jennifergarcia/proyectoda/FormularioActivity.java
+++ app/src/main/java/com/example/jennifergarcia/proyectoda/FormularioActivity.java
@@ -1,10 +1,27 @@
 package com.example.jennifergarcia.proyectoda;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class FormularioActivity extends AppCompatActivity {
+    Button consultarLatLong;
+    EditText edtLat, edtLong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +30,57 @@ public class FormularioActivity extends AppCompatActivity {
         /* Agregar en cada codigo de Java la línea de abajo para centrar el nombre de la app titulo centrado action bar*/
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.txt_titulo);
+
+        consultarLatLong = (Button) findViewById(R.id.botton);
+        edtLat = (EditText) findViewById(R.id.txtLat);
+        edtLong = (EditText) findViewById(R.id.txtLon);
+        getLocalizacion();
+
+    }
+    private void getCargarLocalizacion() {
+        consultarLatLong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LocationManager locationManager = (LocationManager) FormularioActivity.this.getSystemService(Context.LOCATION_SERVICE);
+
+                LocationListener locationListener = new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        edtLat.setText(""+location.getLatitude());
+                        edtLong.setText(""+location.getLongitude());
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+
+                    }
+                };
+                int permiso = ContextCompat.checkSelfPermission(FormularioActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION);
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+                Toast.makeText(FormularioActivity.this, "Ubicación generada con éxito", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    private void getLocalizacion(){
+        int permiso = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if(permiso == PackageManager.PERMISSION_DENIED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            }else{
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+            }
+        }
+
     }
 }
